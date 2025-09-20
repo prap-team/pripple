8252739dfe108934d88312c4bc56ab8a48747532
Parkayun <iamparkayun@gmail.com>
2025-09-20 16:11:52 +0900

    WT

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
 
+try:
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+
 setup(
     name="pripple",
     version="0.1.0",
     author="TartAI Inc",
     author_email="dev@tart.ai.kr",
     description="Ripple wrapper for RWA and Payments",
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/prap-team/pripple",
     project_urls={
@@ -15,10 +22,20 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # 다른 라이선스 사용 가능
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    packages=find_packages(),
+    python_requires=">=3.8",
     install_requires=[
+        "xrpl-py",
     ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-mock>"
+        ]
+    },
+    include_package_data=True,
 )
+
